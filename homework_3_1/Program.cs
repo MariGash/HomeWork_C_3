236ed69d1b752278450b0fae8d6b44ab988230a9
Maria <mgashenina@gmail.com>
2022-12-07 23:00:35 +0300

    homework_3_1

---
+++ homework_3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (9999 < N && N < 100000)
+{
+    int a = N / 10000;
+    int b = N / 1000 % 10;
+    int c = N / 10 % 10;
+    int d = N % 10;
+    if (a == d && b == c)
+    {
+        Console.WriteLine("Число " + N + " является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число " + N + " не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
