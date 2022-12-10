1933847c4e25b2418cf6351d174c6eb13cc900ac
Maria <mgashenina@gmail.com>
2022-12-10 19:33:54 +0300

    homework_3_3 2

--- homework_3_3/Program.cs
+++ homework_3_3/Program.cs
@@ -1,13 +1,24 @@
 ﻿void FillArray(int[] collection)
 {
-    for (int i = 1; i <= collection.Length; i = i+1)
+    for (int i = 1; i < collection.Length; i = i+1)
     {
-        Console.WriteLine("{0}\t{1}", i, i*i*i);
+        collection[i] = i*i*i;
+    }
+    
+        //Console.WriteLine("{0}\t{1}", i, i*i*i);
+}
+void PrintArray(int[] collection)
+{
+    for (int i = 1; i < collection.Length; i = i+1)
+    {
+    Console.WriteLine("{0}\t{1}", i, collection[i]);
     }
 }
+
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Число\tКуб");
 
-int[] array = new int[N];
+int[] array = new int[N+1];
 FillArray(array);
+PrintArray(array);
